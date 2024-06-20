5c238b7038f115eb96d4a29e452b3c975df1bce5
Alexander Gil <pando855@gmail.com>
2024-06-20 18:34:04 +0200

    style: Fix go code with go fmt

--- pkg/kured/kured_test.go
+++ pkg/kured/kured_test.go
@@ -11,46 +11,46 @@ import (
 )
 
 func TestExtractNodeIDsFromAnnotation(t *testing.T) {
-    const KuredNodeLockAnnotation string = "weave.works/kured-node-lock"
+	const KuredNodeLockAnnotation string = "weave.works/kured-node-lock"
 
-    tests := []struct {
-        name     string
-        annotationValue string
-        want     []string
-    }{
-        {
-            name: "single node ID",
-            annotationValue: `{"nodeID":"kind-control-plane2","metadata":{"unschedulable":false},"created":"2024-05-31T06:31:37.441623522Z","TTL":0}`,
-            want: []string{"kind-control-plane2"},
-        },
-        {
-            name: "multiple node IDs",
-            annotationValue: `{"maxOwners":2,"locks":[{"nodeID":"kind-worker2","metadata":{"unschedulable":false},"created":"2024-05-30T11:41:32.735905893Z","TTL":0},{"nodeID":"kind-control-plane","metadata":{"unschedulable":false},"created":"2024-05-30T11:41:49.868231413Z","TTL":0}]}`,
-            want: []string{"kind-worker2", "kind-control-plane"},
-        },
-        {
-            name: "manual lock",
-            annotationValue: `{"nodeID":"manual"}`,
-            want: []string{},
-        },
-    }
+	tests := []struct {
+		name            string
+		annotationValue string
+		want            []string
+	}{
+		{
+			name:            "single node ID",
+			annotationValue: `{"nodeID":"kind-control-plane2","metadata":{"unschedulable":false},"created":"2024-05-31T06:31:37.441623522Z","TTL":0}`,
+			want:            []string{"kind-control-plane2"},
+		},
+		{
+			name:            "multiple node IDs",
+			annotationValue: `{"maxOwners":2,"locks":[{"nodeID":"kind-worker2","metadata":{"unschedulable":false},"created":"2024-05-30T11:41:32.735905893Z","TTL":0},{"nodeID":"kind-control-plane","metadata":{"unschedulable":false},"created":"2024-05-30T11:41:49.868231413Z","TTL":0}]}`,
+			want:            []string{"kind-worker2", "kind-control-plane"},
+		},
+		{
+			name:            "manual lock",
+			annotationValue: `{"nodeID":"manual"}`,
+			want:            []string{},
+		},
+	}
 
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            ds := &v1.DaemonSet{
-                ObjectMeta: metav1.ObjectMeta{
-                    Annotations: map[string]string{
-                        KuredNodeLockAnnotation: tt.annotationValue,
-                    },
-                },
-            }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ds := &v1.DaemonSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						KuredNodeLockAnnotation: tt.annotationValue,
+					},
+				},
+			}
 
-            result, err := kured.ExtractNodeIDsFromAnnotation(ds, KuredNodeLockAnnotation)
+			result, err := kured.ExtractNodeIDsFromAnnotation(ds, KuredNodeLockAnnotation)
 			require.NoError(t, err)
 			require.Equal(t, result, tt.want)
 
-        })
-    }
+		})
+	}
 }
 
 func TestExtractNodeIDsFromAnnotationNoAnnotation(t *testing.T) {
