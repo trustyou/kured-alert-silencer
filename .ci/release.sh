7f6d328208508aaa54e33bfa0e48392b629c011b
Alexander Gil <pando855@gmail.com>
2024-08-22 12:33:25 +0200

    ci: Add script for generating version

---
+++ .ci/release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+print_help() {
+    echo "Usage: $0 v<MAJOR>.<MINOR>.<PATCH>"
+    echo "Example: $0 v1.2.3"
+    exit 1
+}
+
+if [ -z "$1" ]; then
+    echo "Error: No version provided."
+    print_help
+fi
+
+if [[ "$1" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Version is valid: $1"
+else
+    echo "Error: Version format is incorrect."
+    print_help
+fi
+
+VERSION=$1 make update-changelog
+
+git add .
+git commit -m "release: Version $1"
+
+echo "After merging the PR, tag and release are automatically done"
